d5745320aa7ec903070374c0f20d9d93ca76f296
laradoren <48968796+laradoren@users.noreply.github.com>
2022-12-05 20:50:15 +0000

    Deploying to gh-pages from @ laradoren/alina-react-imagesearch03@8e55e94a05ded8efa247a26e0cd0ae9252721fc7 🚀

--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import css from './../styles.module.css';
-
-export const Loader = () => {
-  return <div className={css.Loader}>Loading...</div>;
-};
-
-Loader.propTypes = {};
