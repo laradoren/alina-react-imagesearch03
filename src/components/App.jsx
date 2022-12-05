beb32888a504fbd16e94e2e8c90aa90ff6403630
Alina Halushko <alinahalushko2@gmail.com>
2022-12-05 22:37:27 +0200

    refactoring code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import css from './styles.module.css';
-import { SearchBar } from "./SearchBar/SearchBar";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
-import { Button } from "./Button/Button";
-import { Loader } from "./Loader/Loader";
-import { ImageGalleryApi } from "api/ImageGalleryApi";
+import { SearchBar } from './SearchBar/SearchBar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { ImageGalleryApi } from 'api/ImageGalleryApi';
 
 export class App extends React.Component {
   constructor(props) {
@@ -12,50 +12,59 @@ export class App extends React.Component {
     this.state = {
       images: [],
       pageIndex: 1,
-      searchValue: "",
-      isLoading: false
-     };
+      searchValue: '',
+      isLoading: false,
+    };
     this.onSubmit = this.onSubmit.bind(this);
     this.loadNext = this.loadNext.bind(this);
   }
 
   componentDidMount() {
-    ImageGalleryApi.loadImages().then(
-      res => res.json()
-    ).then(images => this.setState({images: images.hits}));
+    ImageGalleryApi.loadImages()
+      .then(res => res.json())
+      .then(images => this.setState({ images: images.hits }));
   }
 
   onSubmit(opt) {
     opt.preventDefault();
     let formData = new FormData(opt.target);
-    let searchValue = formData.get("search");
+    let searchValue = formData.get('search');
     this.setState({
       ...this.state,
       searchValue: searchValue,
     });
-    ImageGalleryApi.loadImages(searchValue).then(
-      res => res.json()
-    ).then(images => this.setState({images: [...images.hits]}));
+    ImageGalleryApi.loadImages(searchValue)
+      .then(res => res.json())
+      .then(images => this.setState({ images: [...images.hits] }));
   }
 
   loadNext() {
     this.setState({
       ...this.state,
       pageIndex: this.state.pageIndex + 1,
-      isLoading: true
+      isLoading: true,
     });
-    ImageGalleryApi.loadImages(this.state.searchValue, this.state.pageIndex + 1).then(
-      res => res.json()
-    ).then(images => this.setState({images: [...this.state.images, ...images.hits], isLoading: false}));
+    ImageGalleryApi.loadImages(this.state.searchValue, this.state.pageIndex + 1)
+      .then(res => res.json())
+      .then(images =>
+        this.setState({
+          images: [...this.state.images, ...images.hits],
+          isLoading: false,
+        })
+      );
   }
 
-  render () {
+  render() {
     return (
       <div className={css.App}>
         <SearchBar onSubmit={this.onSubmit} />
         <ImageGallery images={this.state.images} />
-        {this.state.isLoading ? <Loader /> : <Button loadNext={this.loadNext}/>}
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <Button loadNext={this.loadNext} />
+        )}
       </div>
     );
   }
-};
+}
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 import css from './../styles.module.css';
 
 export const Loader = () => {
-    return (
-      <div className={css.Loader}>Loading...</div>
-    );
-}
+  return <div className={css.Loader}>Loading...</div>;
+};
 
-Loader.propTypes = {
-}
+Loader.propTypes = {};
